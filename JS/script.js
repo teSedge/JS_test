bbd318fff69ce0faf553dba1a7c347b4166dc3d5
teSedge <teSege99@gmail.com>
2023-04-25 12:08:22 +0400

    v2

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,68 @@
 'use strict';
-let numberOfFilms = confirm('вы уже посмотрели фильм?'),
-    personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
-personalMovieDB['movies'][prompt('film name','')] = prompt('rate','');
+let numberOfFilms, 
+    personalMovieDB = {};
+
+do {
+    numberOfFilms = '';
+    numberOfFilms = prompt('Сколько фильмов вы посмотрели?');
+    if (numberOfFilms === '' || numberOfFilms === null) {
+        alert('Ошибка. Попробуйте снова');
+        continue;
+    }
+    numberOfFilms = +numberOfFilms;
+    if (isNaN(numberOfFilms) || numberOfFilms < 0) {
+        alert('Ошибка. Попробуйте снова');
+        continue;
+    } else {
+        break;
+    }
+} while (true);
+
+if (numberOfFilms > 50) {
+    alert('Вы киноман')
+} else if (numberOfFilms < 10) {
+    alert('Просмотренно довольно мало фильмов')
+} else {
+    alert('Вы классический зритель')
+}
+
+
+personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let film, rate;
+
+for (let i = 0; i < 2; i++) {
+    
+    do {
+        film = prompt('Название фильма','');
+        if (film === '' || film === null || film.length > 50) {
+            alert('Ошибка. Попробуйте снова');
+            continue;
+        } else {
+            break;
+        }
+    } while (true);
+    do {
+        rate = prompt('Оцкека','');
+        if (rate === '' || rate === null) {
+            alert('Ошибка. Попробуйте снова');
+            continue;
+        } else {
+            break;
+        }
+    } while (true);
+    personalMovieDB['movies'][film] = rate;
+}
+
+
+    
+
+
 console.log(personalMovieDB);
 
